7985366e6ab315f97a68ace34e435ff1505651ff
Marcel Katzmann <marcel@katzmann.biz>
2020-07-02 23:34:06 +0200

    Roxygen docu added for fbind function

--- R/main.R
+++ R/main.R
@@ -3,10 +3,11 @@
 # library(devtools) # e.g. load_all()
 #
 #
-# (a <- factor(c("character", "hits", "your", "eyeballs")))
-# (b <- factor(c("but", "integer", "where it", "counts")))
+(a <- factor(c("character", "hits", "your", "eyeballs")))
+(b <- factor(c("but", "integer", "where it", "counts")))
 # c(a,b)
 # factor(c(as.character(a), as.character(b)))
 # use_r("test_funct")
 # load_all()
 # check()
+# R CMD check is the gold standard for checking that an R package is in full working
