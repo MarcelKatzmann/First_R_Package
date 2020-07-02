d8b06a344d95d116261a3811bda3c971aec70537
Marcel Katzmann <marcel@katzmann.biz>
2020-07-02 23:14:17 +0200

    Added function fbind / description file maintained
    - helper function for the conversion from character to factors
    - added author and mail address to the description file

---
+++ R/main.R
@@ -0,0 +1,12 @@
+#use_git()
+# library(usethis) # e.g. use_r()
+# library(devtools) # e.g. load_all()
+#
+#
+# (a <- factor(c("character", "hits", "your", "eyeballs")))
+# (b <- factor(c("but", "integer", "where it", "counts")))
+# c(a,b)
+# factor(c(as.character(a), as.character(b)))
+# use_r("test_funct")
+# load_all()
+# check()
